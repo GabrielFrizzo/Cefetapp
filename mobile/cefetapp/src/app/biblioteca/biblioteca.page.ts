96a2d787dbf299df51ed4d027e910a00b5e7a8c7
Gabriel Frizzo <gabrielfrizzo@gmail.com>
2019-10-22 22:07:44 -0300

    create library page

--- mobile/cefetapp/src/app/biblioteca/biblioteca.page.ts
+++ mobile/cefetapp/src/app/biblioteca/biblioteca.page.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BibliotecaPage implements OnInit {
 
+  public books = [
+    { title: 'A volta dos que não foram', author: 'Aluísio de Azevedo', renewals: 2 },
+    { title: 'Percy Jackson', author: 'Karl Marx', renewals: 1 },
+    { title: 'Física 7', author: 'Rafael Barreto', renewals: 3 },
+    { title: 'Matemática Introdutória', author: 'Desconhecido', renewals: 3 },
+    { title: 'Ionic Para Iniciantes', author: 'Mateus Freitas', renewals: 0 },
+  ];
+
   constructor() { }
 
   ngOnInit() {
