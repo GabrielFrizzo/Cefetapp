3e6ba5ff96a89ef44a67144cac91f18660033a50
Alexandre Nadolni Bonacim <bonacim@alunos.utfpr.edu.br>
2019-12-10 18:26:25 -0300

    integrate biblioteca

--- mobile/cefetapp/src/app/services/api-django.service.ts
+++ mobile/cefetapp/src/app/services/api-django.service.ts
@@ -75,4 +75,23 @@ export class ApiDjangoService {
       });
     });
   }
+
+  get_biblioteca() {
+    return new Promise((resolve, reject) => {
+      this.storage.get('ACCESS_TOKEN').then((token) => {
+        this.http.get(`${this.SERVER_ADRESS}${this.url}books`,
+                         { headers: {
+                            Authorization: `Token ${token}`
+                           }
+                         }).subscribe((result: any) => {
+                           console.log('sem erro');
+                           resolve(result);
+                         },
+                         (error) => {
+                           console.log(error);
+                           reject(error.json);
+                         });
+      });
+    });
+  }
 }
