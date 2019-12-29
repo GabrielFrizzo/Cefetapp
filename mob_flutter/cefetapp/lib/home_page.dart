b6dd43eeae8a25efdc24570b3c876d3a0389520a
Gabriel Frizzo <gabrielfrizzo@gmail.com>
2019-12-29 02:08:45 -0300

    Create mobile Flutter project

---
+++ mob_flutter/cefetapp/lib/home_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, Icon> _pages = {
+      'Grade Horária': Icon(MdiIcons.calendarMonth),
+      'Biblioteca': Icon(MdiIcons.bookshelf),
+      'Histórico': Icon(Icons.school),
+      'Disciplinas': Icon(MdiIcons.bookOpenVariant)
+    };
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.none,
+                alignment: Alignment(-0.8, -2),
+                image: AssetImage('assets/blueCircle.png'),
+              ),
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.all(50),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 25),
+                            child: Text(
+                              'Oi, Gabriela',
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ),
+                          Text('Engenharia de Alimentos'),
+                          Text(
+                            'Registro Acadêmico: 1508920',
+                            style: TextStyle(fontWeight: FontWeight.w300),
+                          ),
+                        ],
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: NetworkImage(
+                        'https://avatars2.githubusercontent.com/u/34910470?s=400&u=522442f28ae28b8e12dd27f93291dbfbc6ca4e39&v=4',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(50),
+                  itemCount: _pages.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    String _key = _pages.keys.elementAt(index);
+                    return Container(
+                      padding: EdgeInsets.all(10),
+                      height: 100,
+                      child: RaisedButton(
+                        color: Theme.of(context).primaryColorLight,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(
+                                _key,
+                                textAlign: TextAlign.left,
+                                style: TextStyle(fontSize: 25),
+                              ),
+                            ),
+                            _pages[_key],
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
