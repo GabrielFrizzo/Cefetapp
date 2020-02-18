b43d02e098ab63a841c82b3068176621733a2043
Gabriel Frizzo <gabrielfrizzo@gmail.com>
2020-02-18 02:57:29 -0300

    Fix button shadow in login page

--- mob_flutter/cefetapp/lib/login_page.dart
+++ mob_flutter/cefetapp/lib/login_page.dart
@@ -24,7 +24,7 @@ class LoginPage extends StatelessWidget {
                     child: Image.asset('assets/images/logo.png', width: 200),
                   ),
                   Container(
-                    height: MediaQuery.of(context).size.height * 0.35,
+                    height: MediaQuery.of(context).size.height * 0.3,
                     decoration: BoxDecoration(
                       color: const Color(0xFFFBFBFB),
                       boxShadow: [
@@ -32,7 +32,10 @@ class LoginPage extends StatelessWidget {
                       ],
                       borderRadius: BorderRadius.circular(30),
                     ),
-                    margin: const EdgeInsets.symmetric(horizontal: 40),
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 40,
+                      vertical: 20,
+                    ),
                     padding: const EdgeInsets.symmetric(
                       horizontal: 30,
                       vertical: 20,
@@ -83,7 +86,7 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                   Container(
-                    height: 200,
+                    height: MediaQuery.of(context).size.height * 0.25,
                     margin: const EdgeInsets.symmetric(horizontal: 60),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -135,15 +138,13 @@ class LoginPage extends StatelessWidget {
   }
 
   Widget buildLowerButton({String text, Function onPressed}) {
-    return FlatButton(
-      onPressed: onPressed,
-      child: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.symmetric(vertical: 20),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: const Color(0xFF5887A9),
-        ),
+    return Container(
+      width: 500,
+      child: FlatButton(
+        padding: const EdgeInsets.symmetric(vertical: 23),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        color: const Color(0xFF5887A9),
+        onPressed: onPressed,
         child: Text(
           text,
           style: TextStyle(color: Colors.white, fontSize: 23),
